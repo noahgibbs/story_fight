70167854c1dfdf83799f57b4c9eeb35491d3bcc6
Noah Gibbs <noah_gibbs@yahoo.com>
2013-04-08 21:52:02 -0700

    Sketch the first extremely basic meta-story

--- lib/story_fight.rb
+++ lib/story_fight.rb
@@ -46,17 +46,93 @@ module StoryFight
     # each role.
     #
     class Meta
+      attr_reader :details
+
+      def initialize(details = {})
+        @details = details
+      end
+
+      def bind(world, bound_details = {})
+        # Everything matches, just return it
+        if bound_details.keys.sort == @details.keys.sort
+          return [ bound_details ]
+        end
+
+        # This isn't really an "each".  It's actually
+        # a "pick the first unbound detail."
+        #
+        @details.each do |name, conditions|
+          # Assume existing bindings are correct
+          next if bound_details[name]
+
+          method_name = conditions[:is].to_s + "s"
+          obj_list = world.send(method_name)
+
+          spec = [conditions[:spec] || []].flatten
+
+          did_bind = false
+          bindings = []
+          obj_list.each do |candidate|
+            # If we got a match on "spec"...
+            if (candidate.adjectives & spec).size == spec.size
+              did_bind = true
+              # Intentionally copy, not modify, into new_bound
+              new_bound = bound_details.merge(name => candidate)
+              bindings += bind(world, new_bound)
+            end
+          end
+
+          return bindings if did_bind
+
+          # Otherwise, no match
+          return []
+        end
+      end
     end
   end
 
   class World
     attr_reader :places, :actors, :things, :actions
 
+    def initialize(options = {})
+      @places = []
+      @actors = []
+      @things = []
+      @actions = []
+    end
+
+    def new_actor(options = {})
+      @actors << Actor.new(options)
+      @actors[-1]
+    end
+
+    def new_thing(options = {})
+      @things << Thing.new(options)
+      @things[-1]
+    end
+
+    def new_place(options = {})
+      @places << Place.new(options)
+      @places[-1]
+    end
+
+    def new_action(options = {})
+      @actions << Action.new(options)
+      @actions[-1]
+    end
+
     # A Specifiable object can have specifics attached to it that are
     # germane to the story.
     #
     module Specifiable
-      attr_reader :adjectives
+      attr_reader :adjectives, :desc
+
+      def initialize(options = {})
+        @adjectives = options.delete(:spec) || []
+        @desc = options.delete(:desc) || :NoDesc
+
+        super()
+      end
     end
 
     class Place
--- test/sketch_test.rb
+++ test/sketch_test.rb
@@ -5,9 +5,31 @@ class SketchTest < MiniTest::Unit::TestCase
 
   def setup
     @world = World.new
+
+    @joe = @world.new_actor :desc => :Joe, :spec => [ :man ]
+
+    @sam = @world.new_actor :desc => :Sam
+    @bob = @world.new_actor :desc => :Bob, :spec => [ :man, :green ]
+
+    @burp = @world.new_action :desc => :Burp, :spec => [ :involuntary ]
+    @sneeze = @world.new_action :desc => :Sneeze, :spec => [ :involuntary ]
+    @whistle = @world.new_action :desc => :Whistle, :spec => [ :musical ]
+
+    @meta1 = Story::Meta.new :obj1 => { :desc => :the_man, :is => :actor, :spec => :man },
+      :action1 => { :is => :action, :spec => :involuntary }
   end
 
-  def test_truth
-    assert true
+  def test_basic_bind
+    # We should get a binding of all "man" actors (joe, bob) to all
+    # "involuntary" actions (burp, sneeze).
+
+    # For the comparison, sort on actor desc then action desc
+    assert_equal [
+      { :obj1 => @bob, :action1 => @burp },
+      { :obj1 => @bob, :action1 => @sneeze },
+      { :obj1 => @joe, :action1 => @burp },
+      { :obj1 => @joe, :action1 => @sneeze },
+    ],
+      @meta1.bind(@world).sort_by { |binding| [binding[:obj1].desc, binding[:action1].desc] }
   end
 end
